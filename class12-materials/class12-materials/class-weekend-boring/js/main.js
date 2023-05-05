b81cfbc89daa5d92e73ca87d667980d398c5c778
ad3rinto <aad3rinto@gmail.com>
2023-05-05 11:27:17 +0100

    solve simple class-weekend-boring challenge

--- class12-materials/class12-materials/class-weekend-boring/js/main.js
+++ class12-materials/class12-materials/class-weekend-boring/js/main.js
@@ -2,9 +2,16 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase();
 
   //Conditionals go here
 
+  if (day === "Tuesday".toLowerCase() || day === "Thursday".toLowerCase()) {
+    document.querySelector("#placeToSee").innerHTML = "Course day";
+  } else if (day === "Monday".toLowerCase() || day === "Wednesday".toLowerCase() || day === "Friday".toLowerCase()) {
+    document.querySelector("#placeToSee").innerHTML = "Typical Week Day";
+  } else {
+    document.querySelector("#placeToSee").innerHTML = "Weekend Yayyyy!!!!!";
 
+  }
 }
