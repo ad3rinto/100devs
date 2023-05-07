b923575fe4e55e96807b58118b9a026659031ae5
ad3rinto <aad3rinto@gmail.com>
2023-05-07 12:35:31 +0100

    solved temperature conversion challenge

--- class13-solutions/class13-materials/temperature-converter/js/main.js
+++ class13-solutions/class13-materials/temperature-converter/js/main.js
@@ -1,14 +1,61 @@
 //Write your pseduo code first! 
-//0 -> 32
-document.querySelector('#yell').addEventListener('click', convert)
 
-function convert() {
-    //need the value that is in selseeyus
-    let temp = document.querySelector('#sel').value
-    //convert from selseeyus to fairinheight
-    temp = temp * 9/5 + 32
-    //show it
-    document.querySelector('#placeToYell').innerText = temp
+
+//Throw in an even handler
+document.querySelector("#yell").addEventListener('click', transform);
+
+
+
+
+function transform () {
+    //Obtain input
+    const celciusTemp = Number(document.querySelector("#temp").value);
+    //transform from celcius to Farenheit
+    const converterTemp = (celciusTemp*9/5) + 32
+    console.log(converterTemp)
+    // display answer in DOM
+    document.querySelector("#placeToYell").innerText = `The converted temperature is ${converterTemp}F degs` ;
+} 
+    
+
+
+//through in some validation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//0 -> 32
+// document.querySelector('#yell').addEventListener('click', convert)
+
+// function convert() {
+//     //need the value that is in selseeyus
+//     let temp = document.querySelector('#sel').value
+//     //convert from selseeyus to fairinheight
+//     temp = temp * 9/5 + 32
+//     //show it
+//     document.querySelector('#placeToYell').innerText = temp
   
-}
+// }
 
