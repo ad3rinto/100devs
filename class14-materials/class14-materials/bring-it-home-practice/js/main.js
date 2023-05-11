c673bfea70b5cda21b9ea345d789f9c0977b3975
ad3rinto <aad3rinto@gmail.com>
2023-05-11 19:16:16 +0100

    solved the rest of class14 bring-it-home challenge

--- class14-materials/class14-materials/bring-it-home-practice/js/main.js
+++ class14-materials/class14-materials/bring-it-home-practice/js/main.js
@@ -1,14 +1,37 @@
 // *Variables*
 // Create a variable and console log the value
+// const age = 50;
+// alert(age);
 
 // Create a variable, add 10 to it, and alert the value
+// let cost = 1000;
+// alert(cost += 10);
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
+// subtractFour = (a, b, c, d) => {
+//     alert(a - b - c - d);
 
+// }
 // Create a function that divides one number by another and returns the remainder
+function findMod(p, y) {
+    return (p % y)
+}
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
-
+addFifty = (r, g) => {
+    if ((r + g) > 50) {
+        alert("Agbako")
+    } else {
+        alert("BS")
+    }
+}
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+function checkMulti(j, k, l) {
+    if ((j * k * l) % 3 === 0) {
+        console.log("Yes")
+    } else {
+        console.log("No")
+    }
+}
