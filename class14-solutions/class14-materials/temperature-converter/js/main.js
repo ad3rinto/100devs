e5e5d7701cf1493315a2b8da3350c0b31e1c5ba7
A <aad3rinto@gmail.com>
2023-05-20 19:58:03 +0100

    challenges

--- class14-solutions/class14-materials/temperature-converter/js/main.js
+++ class14-solutions/class14-materials/temperature-converter/js/main.js
@@ -1,14 +1,30 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
 
-//only on click
-document.querySelector('#yell').addEventListener('click', run)
 
-function run() {
-    console.log('HELLO TWITCH')
-    //get value out of input
-    let pumpkin = document.querySelector('#firstName').value
-    //convert the value
-    pumpkin = pumpkin * 9/5 + 32
-    //show the value to user
-    document.querySelector('#placeToYell').innerText = pumpkin
-  }
+
+
+
+document.querySelector('#convert').addEventListener('click', convertIt);
+
+function convertIt() {
+
+  let num = document.querySelector('#temp').value
+  console.log(num);
+  document.querySelector('#placeToConvert').innerText = `Calculated value is ${num + 31}`
+}
+
+
+
+
+// //only on click
+// document.querySelector('#yell').addEventListener('click', run)
+
+// function run() {
+//   console.log('HELLO TWITCH')
+//   //get value out of input
+//   let pumpkin = document.querySelector('#firstName').value
+//   //convert the value
+//   pumpkin = pumpkin * 9 / 5 + 32
+//   //show the value to user
+//   document.querySelector('#placeToYell').innerText = pumpkin
+// }
