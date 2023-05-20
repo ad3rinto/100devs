ea4c9e136ed885f9dc779ce89d5299c2c741be36
A <aad3rinto@gmail.com>
2023-05-20 20:08:31 +0100

    nfi

--- class14-materials/class14-materials/temperature-converter/js/main.js
+++ class14-materials/class14-materials/temperature-converter/js/main.js
@@ -2,6 +2,15 @@
 // Obtain the desired value 
 
 // store the value 
+const inputValue = document.querySelector("#source").value
+
+
+const convert = (inputValue) => {
+    console.log(inputValue)
+}
+
+document.querySelector('#butt').addEventListener('click', convert)
+
 
 // convert the value
 
