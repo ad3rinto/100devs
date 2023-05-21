ceb6c32295908d3baa1ead391fe7723c9e2d3f16
A <aad3rinto@gmail.com>
2023-05-21 12:05:04 +0100

    added a function to display all contestants

--- class14-solutions/class14-materials/bachelor-code/2/js/main.js
+++ class14-solutions/class14-materials/bachelor-code/2/js/main.js
@@ -2,24 +2,32 @@ const andi = document.querySelector('#andi')
 const claire = document.querySelector('#claire')
 const sharleen = document.querySelector('#sharleen')
 
+
 document.querySelector('#andiNext').addEventListener('click', andiNext)
 document.querySelector('#claireNext').addEventListener('click', claireNext)
 document.querySelector('#sharleenNext').addEventListener('click', sharleenNext)
+document.querySelector('#all').addEventListener('click', allDem)
 
-function andiNext(){
+function andiNext() {
 	claire.classList.add('hidden')
 	sharleen.classList.add('hidden')
 	andi.classList.toggle('hidden')
 }
 
-function claireNext(){
+function claireNext() {
 	andi.classList.add('hidden')
 	sharleen.classList.add('hidden')
 	claire.classList.toggle('hidden')
 }
 
-function sharleenNext(){
+function sharleenNext() {
 	claire.classList.add('hidden')
 	andi.classList.add('hidden')
 	sharleen.classList.toggle('hidden')
+}
+
+function allDem() {
+	claire.classList.toggle('hidden')
+	sharleen.classList.toggle('hidden')
+	andi.classList.toggle('hidden')
 }
