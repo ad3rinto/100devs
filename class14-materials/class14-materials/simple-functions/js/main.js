960396a6aba8d692024bbf4aaac5705f5cb46396
ad3rinto <aad3rinto@gmail.com>
2023-05-26 20:39:57 +0100

    awalokan

--- class14-materials/class14-materials/simple-functions/js/main.js
+++ class14-materials/class14-materials/simple-functions/js/main.js
@@ -1,5 +1,11 @@
 //create a function that adds two numbers and alerts the sum
+function addNum(num1, num2) {
+    alert(num1 + num2)
+}
 
 //create a function that multiplys three numbers and console logs the product
+const multNumbers = (c, d, e) => {
+    console.log(c * d * e)
 
+}
 //create a function that divides two numbers and returns the ???
