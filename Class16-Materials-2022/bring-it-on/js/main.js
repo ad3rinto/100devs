d4ed067de4453c1729bd619786aafb3cc58a705c
ad3rinto <aad3rinto@gmail.com>
2023-05-27 21:40:59 +0100

    solve bring-it-on challenge

--- Class16-Materials-2022/bring-it-on/js/main.js
+++ Class16-Materials-2022/bring-it-on/js/main.js
@@ -1,17 +1,39 @@
 // *Variables*
 // Create a variable and console log the value
+// const newText = "welcome";
+// console.log(newText)
 
 // Create a variable, add 10 to it, and alert the value
+// const numOfText = 23;
+// alert(numOfText + 10);
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
+// const subtractFour = (a, b, c, d) => {
+//     sum = a - b - c - d;
+//     alert(sum)
+// }
+// subtractFour(122, 22, 29, 2)
 
 // Create a function that divides one number by another and returns the remainder
-
+// function findModulo(e, f) {
+//     return (e % f)
+// }
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
-
+function alert50(g, h) {
+    if ((g + h) > 50) {
+        alert("Jumanji")
+    } else {
+        console.log("nothing dey there")
+    }
+}
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
 
 //*Loops*
 //Create a function that takes in a word and a number. Console log the word x times where x was the number passed in
+function takeWord(k, word) {
+    for (let i = 0; i < k; i++) {
+        console.log(i, word)
+    }
+}
