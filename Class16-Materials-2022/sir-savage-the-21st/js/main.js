e62f82de47a648309cb38ae7703600974f06ba50
ad3rinto <aad3rinto@gmail.com>
2023-05-30 17:56:01 +0100

    refactored to clear paragraphs before next re-render

--- Class16-Materials-2022/sir-savage-the-21st/js/main.js
+++ Class16-Materials-2022/sir-savage-the-21st/js/main.js
@@ -1,6 +1,7 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 
 const callSavage = (a) => {
+    clearPage()
     for (let i = 0; i < a; i++) {
         const para = document.createElement('p')
         const node = document.createTextNode('21');
@@ -10,5 +11,13 @@ const callSavage = (a) => {
 
 
 }
-callSavage(5)
+// callSavage(15)
 //Bonus can you make it print '21' 21 times to the dom?
+
+function clearPage() {
+    const list = document.querySelector("#savageSays")
+    while (list.hasChildNodes()) {
+        list.removeChild(list.firstChild);
+    }
+
+}
