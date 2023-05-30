f16a8a9d968852f78a134780707fb6adb52ef162
ad3rinto <aad3rinto@gmail.com>
2023-05-30 17:40:00 +0100

    solve sir savage challenge

--- Class16-Materials-2022/sir-savage-the-21st/js/main.js
+++ Class16-Materials-2022/sir-savage-the-21st/js/main.js
@@ -1,2 +1,14 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
+
+const callSavage = (a) => {
+    for (let i = 0; i < a; i++) {
+        const para = document.createElement('p')
+        const node = document.createTextNode('21');
+        para.appendChild(node)
+        document.querySelector("#savageSays").appendChild(para);
+    }
+
+
+}
+callSavage(5)
 //Bonus can you make it print '21' 21 times to the dom?
