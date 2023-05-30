4f336ab12310a4202d14ad641765339349b71ee8
ad3rinto <aad3rinto@gmail.com>
2023-05-30 20:05:01 +0100

    solve stop snacking challenge

--- Class16-Materials-2022/stop-snacking/js/main.js
+++ Class16-Materials-2022/stop-snacking/js/main.js
@@ -1,1 +1,16 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
+
+
+
+const blaster = () => {
+    const num1 = document.querySelector('input').value;
+    let i = 0;
+    document.querySelector("#stops").innerHTML = ""
+    while (i < num1) {
+        document.querySelector("#stops").innerHTML += `  Stop snacking!!`
+        i++
+    }
+}
+
+
+document.querySelector("#help").addEventListener("click", blaster)
