2603189b3a2372d30ad72c9f1a7f4e47ad5dfc43
ad3rinto <aad3rinto@gmail.com>
2023-06-01 21:48:37 +0100

    added image dynamically

--- SuperReview-2022/objects-dog-photos/js/main.js
+++ SuperReview-2022/objects-dog-photos/js/main.js
@@ -1,11 +1,23 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
 const url = "https://dog.ceo/api/breeds/image/random";
 
+
+
+handleClick = () => {
+    document.querySelector("#test").innerHTML = "<h2>Hello</h2>";
+}
+
+document.querySelector('h1').addEventListener('click', handleClick);
+
+
 async function fetchDogLists() {
     const response = await fetch(url);
     const jsonData = await response.json();
     let image = jsonData["message"]
     console.log(image);
+    let dogImages = document.createElement('IMG');
+    dogImages.src = image;
+    document.querySelector('#test').append(dogImages)
     document.querySelector("img").src = image;
 }
 
