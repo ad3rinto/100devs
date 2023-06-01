b3e4fd34022f60605bcff95c60ddeb5b6a7d58a2
ad3rinto <aad3rinto@gmail.com>
2023-06-01 20:46:24 +0100

    fetched dog image from dog.ceo api and placed image in the DOM

--- SuperReview-2022/objects-dog-photos/js/main.js
+++ SuperReview-2022/objects-dog-photos/js/main.js
@@ -1,1 +1,12 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+const url = "https://dog.ceo/api/breeds/image/random";
+
+async function fetchDogLists() {
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    let image = jsonData["message"]
+    console.log(image);
+    document.querySelector("img").src = image;
+}
+
+fetchDogLists();
