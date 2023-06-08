79417b2f655b28d8a456fb62840c54b13972ee16
ad3rinto <aad3rinto@gmail.com>
2023-06-08 17:52:42 +0100

    savage challenge

--- SuperReview-2022/sir-savage-the-21st/js/main.js
+++ SuperReview-2022/sir-savage-the-21st/js/main.js
@@ -1,2 +1,16 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 //Bonus can you make it print '21' 21 times to the dom?
+// let numberOfLoops = 0;
+
+// function check(numberOfLoops) {
+//     while (numberOfLoops < 21) {
+//         console.log(21)
+//         numberOfLoops++
+//     }
+
+// }
+function check2() {
+    for (let num = 0; num < 21; num++) {
+        document.querySelector("#savageSays").innerHTML += 21
+    }
+}
