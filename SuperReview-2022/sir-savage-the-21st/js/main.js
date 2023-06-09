0b92a27c9faa4a0ad65068c80d6af32b85c780bc
ad3rinto <aad3rinto@gmail.com>
2023-06-09 21:11:59 +0100

    solved fizzbuzz challenge

--- SuperReview-2022/sir-savage-the-21st/js/main.js
+++ SuperReview-2022/sir-savage-the-21st/js/main.js
@@ -14,3 +14,18 @@ function check2() {
         document.querySelector("#savageSays").innerHTML += " 21"
     }
 }
+
+const loopFinder = (a) => {
+    for (let i = 1; i <= a; i++) {
+        if ((i % 5 == 0) && (i % 3 == 0)) {
+            console.log("fizzbuzz")
+        } else if (i % 5 === 0) {
+            console.log("buzz")
+        } else if (i % 3 == 0) {
+            console.log("fizz")
+        } else {
+            console.log(i)
+        }
+    }
+
+}
