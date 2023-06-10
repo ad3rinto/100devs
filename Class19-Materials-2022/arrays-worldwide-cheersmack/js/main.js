c0d2369ff39ad9dccfc7cc26e4d7127819eddfc7
ad3rinto <aad3rinto@gmail.com>
2023-06-10 22:55:27 +0100

    solved arrays worldwide cheersmack challenge

--- Class19-Materials-2022/arrays-worldwide-cheersmack/js/main.js
+++ Class19-Materials-2022/arrays-worldwide-cheersmack/js/main.js
@@ -1,1 +1,13 @@
-//Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
+//Create a function that takes in a number. 
+// The function should return an array that contains every number from 1 to that number as seperate elements
+
+function createArray(num) {
+    let setOfArrays = [];
+    for (let i = 1; i <= num; i++) {
+        setOfArrays.push(i)
+    }
+    console.log(setOfArrays)
+}
+
+
+createArray(113)
