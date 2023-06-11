393808d5fc604990edf14b249c209a8e9a817a31
ad3rinto <aad3rinto@gmail.com>
2023-06-11 09:03:10 +0100

    refactored

--- Class20-Materials-2022/arrays-worldwide-cheersmack/js/main.js
+++ Class20-Materials-2022/arrays-worldwide-cheersmack/js/main.js
@@ -1,1 +1,18 @@
-//Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
+//Create a function that takes in a number. 
+//The function should return an array that contains every number from 1 
+//to that number as separate elements
+
+
+const numSeperator = (num) => {
+    let numList = [];
+    let i = 1;
+    while (i <= num) {
+        numList.push(i)
+        i++;
+    }
+
+    console.log(numList)
+
+}
+
+numSeperator(8)
