634b99b0426cdb702ad3ca3b9a674aef0806e82f
ad3rinto <aad3rinto@gmail.com>
2023-06-12 19:34:11 +0100

    solve summer games challenge

--- Class20-Materials-2022/review-summer-games/js/main.js
+++ Class20-Materials-2022/review-summer-games/js/main.js
@@ -1,1 +1,13 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+
+const arrayChomper = (arr1) => {
+    let answer = 1;
+    arr1.forEach(element => {
+        answer *= element
+
+    });
+    console.log(answer)
+}
+
+
+arrayChomper([1, 2, 3, 4, 10])
