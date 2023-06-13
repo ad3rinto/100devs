e9b295d80fc01a659cab1135196d7f8ef1f7ed6d
ad3rinto <aad3rinto@gmail.com>
2023-06-13 18:33:17 +0100

    solved review winter games challenge

--- Class20-Materials-2022/review-winter-games/js/main.js
+++ Class20-Materials-2022/review-winter-games/js/main.js
@@ -1,1 +1,16 @@
-//Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+//Create a function that takes in an array of numbers. 
+//Return a new array containing every even number from the original array (do not use map or filter)
+
+let newList = [];
+
+function arraySelector(arr1) {
+    arr1.forEach(element => {
+        if (element % 2 == 0) {
+            newList.push(element)
+        }
+    });
+    console.log(newList)
+
+}
+
+arraySelector([1, 2, 3, 4, , 23, 32, 11, 13, 446, 676, 87, 89, 22, 12, 21, 35, 6, 7, 8, 9, 10])
