190c84c866fd4ffca0dc8f8e7c32bc26429b8fbc
ad3rinto <aad3rinto@gmail.com>
2023-06-16 19:09:30 +0100

    review constructrs

--- Class22-Materials-2022/objects-lost-galaxy/js/main.js
+++ Class22-Materials-2022/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
 //Create a mouse object that has four properties and three methods
+const mouse = {
+    colour: "brown",
+    speed: "fast",
+    eyeColour: "brown",
+    age: 2,
+
+    sound() {
+        alert("squeal")
+    },
+
+    makeMusic() {
+        console.log("Tra la la la la la")
+    },
+
+    makeQuiet() {
+        console.log("sheeeee")
+    }
+}
