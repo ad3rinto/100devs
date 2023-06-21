d7560a46a3540154190d8c54ea1107691027939c
ad3rinto <aad3rinto@gmail.com>
2023-06-21 17:56:10 +0100

    solve space review class 23

--- Class23-Materials-2022/space-review/js/main.js
+++ Class23-Materials-2022/space-review/js/main.js
@@ -2,11 +2,37 @@
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
 
+//using foreach method
+const myArray = [5, 34, 1000]
+let sumOfArray = 0
+myArray.forEach((num) =>
+    sumOfArray += num)
+
+// alert(sumOfArray)
+
+//using array.reduce
+
+let change = myArray.reduce((acc, num) => acc += num)
+// console.log(change)
+
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+
+const squearer = (arr) => {
+    return (arr.map((item) => item ** 2))
+}
+
+
 
 //Create a function that takes string
 //Print the reverse of that string to the console
 
+const stringer = (str) => {
+    let len = str.length;
+    let newStr = "";
+    for (let i = len; i >= 0; i--) {
+        newStr += str[i]
+    } console.log(newStr)
+}
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
