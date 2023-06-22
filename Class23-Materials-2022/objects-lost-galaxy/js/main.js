8c0ad93a0e116e9ad3cdcc6157947b2315fc9961
ad3rinto <aad3rinto@gmail.com>
2023-06-22 19:13:52 +0100

    create new dog object, with 4 properties and 2 methods

--- Class23-Materials-2022/objects-lost-galaxy/js/main.js
+++ Class23-Materials-2022/objects-lost-galaxy/js/main.js
@@ -1,1 +1,15 @@
 //Create a dog object that has four properties and three methods
+const Dog = {
+    age: 7,
+    colour: "white",
+    breed: "alsatian",
+    temperament: "mild",
+
+    sound: function () {
+        console.log("bark bark")
+    },
+
+    bark: function () {
+        alert("WOOF WOOF")
+    }
+}
