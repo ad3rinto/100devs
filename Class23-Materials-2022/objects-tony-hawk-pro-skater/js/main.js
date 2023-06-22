8bddf0ac74daabc411dbe5b5efefb334f0550734
ad3rinto <aad3rinto@gmail.com>
2023-06-22 21:21:54 +0100

    switch to class syntax

--- Class23-Materials-2022/objects-tony-hawk-pro-skater/js/main.js
+++ Class23-Materials-2022/objects-tony-hawk-pro-skater/js/main.js
@@ -1,27 +1,50 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
-function Skater(bladeType, colour, power, speed) {
-    this.bladeType = bladeType
-    this.colour = colour
-    this.power = power
-    this.speed = speed
+// function Skater(bladeType, colour, power, speed) {
+//     this.bladeType = bladeType
+//     this.colour = colour
+//     this.power = power
+//     this.speed = speed
 
-    this.sound = function () {
-        console.log("Vroooooooooooom")
+//     this.sound = function () {
+//         console.log("Vroooooooooooom")
+//     }
+
+//     this.jump = function () {
+//         console.log("HOw high")
+//     }
+
+//     this.slide = function () {
+//         console.log("How long")
+//     }
+
+// }
+
+class Fighter {
+    constructor(bladeType, colour, power, speed) {
+        this.bladeType = bladeType
+        this.colour = colour
+        this.power = power
+        this.speed = speed
+
+        this.sound = function () {
+            console.log("Vroooooooooooom")
+        }
+
+        this.jump = function () {
+            console.log("HOw high")
+        }
+
+        this.slide = function () {
+            console.log("How long")
+        }
+
     }
-
-    this.jump = function () {
-        console.log("HOw high")
-    }
-
-    this.slide = function () {
-        console.log("How long")
-    }
-
 }
 
 
-let captain = new Skater("inline", "greenish-blue", 70, 120)
-let admiral = new Skater("4-wheels", "Orange", 79, 110)
+
+let captain = new Fighter("inline", "greenish-blue", 70, 120)
+let admiral = new Fighter("4-wheels", "Orange", 79, 110)
 
 console.log(captain)
 console.log(admiral)
