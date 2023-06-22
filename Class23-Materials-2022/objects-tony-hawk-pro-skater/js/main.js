fb6c9d6b9830e33cd0854635bc096c49f227d0fe
ad3rinto <aad3rinto@gmail.com>
2023-06-22 20:57:25 +0100

    created new constructor Skater

--- Class23-Materials-2022/objects-tony-hawk-pro-skater/js/main.js
+++ Class23-Materials-2022/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,27 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+function Skater(bladeType, colour, power, speed) {
+    this.bladeType = bladeType
+    this.colour = colour
+    this.power = power
+    this.speed = speed
+
+    this.sound = function () {
+        console.log("Vroooooooooooom")
+    }
+
+    this.jump = function () {
+        console.log("HOw high")
+    }
+
+    this.slide = function () {
+        console.log("How long")
+    }
+
+}
+
+
+let captain = new Skater("inline", "greenish-blue", 70, 120)
+let admiral = new Skater("4-wheels", "Orange", 79, 110)
+
+console.log(captain)
+console.log(admiral)
