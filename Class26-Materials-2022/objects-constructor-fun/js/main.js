adfc3443e793cbb685af7658ed081b1dfc6f3a64
ad3rinto <aad3rinto@gmail.com>
2023-06-27 14:46:48 +0100

    defined fighter constructor/class

--- Class26-Materials-2022/objects-constructor-fun/js/main.js
+++ Class26-Materials-2022/objects-constructor-fun/js/main.js
@@ -1,1 +1,22 @@
 //Create a constructor with 4 properties and 3 methods
+
+class Fighter {
+    constructor(power, speed, name, specialMove) {
+        this.power = power
+        this.speed = speed
+        this.name = name
+        this.specialMove = specialMove
+
+        this.shout = function () {
+            console.log("Ahuuaaaaaaaa")
+        }
+
+        this.jump = function () {
+            console.log("How high")
+        }
+
+        this.kill = function () {
+            console.log(`Killed with my ${this.specialMove}`)
+        }
+    }
+}
