32e5bf256b8c957dcbdd2292c9a0751a1e7912d2
ad3rinto <aad3rinto@gmail.com>
2023-06-28 11:15:24 +0100

    Obtain drinks details from cocktail db api

--- Class26-Materials-2022/objects-shots/js/main.js
+++ Class26-Materials-2022/objects-shots/js/main.js
@@ -1,1 +1,29 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+
+
+let inputData = document.querySelector("input").value
+
+
+const populate = () => {
+
+    let inputData = document.querySelector("input").value
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputData}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.drinks[0])
+            document.querySelector("h2").innerText = data.drinks[0].strDrink;
+            document.querySelector("p").innerText = data.drinks[0].strInstructions;
+            document.querySelector("img").src = data.drinks[0].strDrinkThumb;
+
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+
+
+
+}
+
+
+document.querySelector("button").addEventListener("click", populate)
