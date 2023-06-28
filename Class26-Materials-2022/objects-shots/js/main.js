814595ba18683c50d9ec64351efa26bd07d44df5
ad3rinto <aad3rinto@gmail.com>
2023-06-28 18:31:06 +0100

    solved - able to clear/refresh page before next event

--- Class26-Materials-2022/objects-shots/js/main.js
+++ Class26-Materials-2022/objects-shots/js/main.js
@@ -4,8 +4,25 @@
 // let inputData = document.querySelector("input").value
 
 
-const populate = () => {
+// function to refresh page upon button click
+const clearDrinksList = () => {
+    const box1 = document.querySelectorAll("h2");
+    box1.forEach(box => {
+        box.remove()
+    })
+    const box2 = document.querySelectorAll("p");
+    box2.forEach(box => {
+        box.remove()
+    })
+    const box3 = document.querySelectorAll("img");
+    box3.forEach(box => {
+        box.remove()
+    })
+}
 
+// function to enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+const populate = () => {
+    clearDrinksList()
     let inputData = document.querySelector("input").value
     const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputData}`;
     fetch(url)
@@ -19,10 +36,10 @@ const populate = () => {
                 let inNode = document.createElement("p")
                 inNode.innerHTML = "<p>" + data.drinks[i].strInstructions + "</p>";
                 node.appendChild(inNode)
-                document.getElementById("here").appendChild(node)
+                document.querySelector("#here").appendChild(node)
                 let image = document.createElement("img")
                 image.src = data.drinks[i].strDrinkThumb
-                document.getElementById("here").appendChild(image)
+                document.querySelector("#here").appendChild(image)
                 // document.querySelector("h2").innerText = data.drinks[i].strDrink;
                 // document.querySelector("p").innerText = data.drinks[i].strInstructions;
                 // document.querySelector("img").src = data.drinks[i].strDrinkThumb;
@@ -37,6 +54,5 @@ const populate = () => {
 
 
 }
-
 
 document.querySelector("button").addEventListener("click", populate)
