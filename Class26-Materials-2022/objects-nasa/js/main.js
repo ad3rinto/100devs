181a182e7c9b963f9098960880b1e41bdfc1bd05
ad3rinto <aad3rinto@gmail.com>
2023-06-29 18:17:21 +0100

    first failed attempt at fetching data from NASA

--- Class26-Materials-2022/objects-nasa/js/main.js
+++ Class26-Materials-2022/objects-nasa/js/main.js
@@ -1,1 +1,19 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+const api_key = "wdxVy89eEPHYi2rNphffgTQr8dN2EPiDD9U2vK9F";
+const todayDate = new Date()
+console.log(todayDate)
+
+const getPhoto = () => {
+
+    const url = `https://api.nasa.gov/planetary/apod`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+
+}
