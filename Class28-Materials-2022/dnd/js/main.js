d587b89fccbd2415b482ead0922fe50dd599e963
ad3rinto <aad3rinto@gmail.com>
2023-07-06 15:29:09 +0100

    solved dnd API challenge

--- Class28-Materials-2022/dnd/js/main.js
+++ Class28-Materials-2022/dnd/js/main.js
@@ -1,17 +1,26 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
 document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+function getFetch() {
+    const choice = document.querySelector('input').value
+    const url = `https://www.dnd5eapi.co/api/spells/${choice}`
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-       
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+            document.querySelector("h3 + p").innerText = data.classes[0].name
+            document.querySelector("h2").innerText = data.classes[0].name
+            const subs = data.subclasses
+            subs.forEach(sub => {
+                let mailList = document.querySelector("ul")
+                let listNode = document.createElement("li")
+                listNode.appendChild(document.createTextNode(sub.name))
+                mailList.appendChild(listNode)
+
+            });
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
 }
 
