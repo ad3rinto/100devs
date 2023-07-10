fd606b5ddf7fbf051f194669bd31d58cd3cffbce
ad3rinto <aad3rinto@gmail.com>
2023-07-10 16:42:16 +0100

    solved the loop, function and var challenge

--- Class29-Materials-2022/boulder-badge/js/main.js
+++ Class29-Materials-2022/boulder-badge/js/main.js
@@ -7,12 +7,28 @@
 //  evolve (rare candies increase your level by one). Finally, create a fourth variable named totalCandies
 //   that sums all the rare candies you would need.
 
+let bulsbasaurRc = 16 - 5;
+let caterpieRc = 7 - 1;
+let weedleRc = 7 - 1;
+
+let totalCandies = bulsbasaurRc + caterpieRc + weedleRc;
+console.log(totalCandies)
+
 //Conditionals And Functions
 //You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius.
 // Create one function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not
 // charmander can battle
 
+function tempConverter(temp) {
+    return ((5 / 9) * (temp - 32))
+}
 //Loops
-//You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon.
+//You have joined an underground pokemon league. In this league, trainers can use any number of pokemon.
 // Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are
 // battling has in their party
+function printLoop(num) {
+    while (num > 0) {
+        console.log("Pikachi I choose you", num)
+        num--
+    }
+}
