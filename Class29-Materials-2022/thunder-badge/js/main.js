131f2f34ab391c6a87788b01a7a9355e075e0bd0
ad3rinto <aad3rinto@gmail.com>
2023-07-10 22:54:29 +0100

    created new pokemon class using the class constructor

--- Class29-Materials-2022/thunder-badge/js/main.js
+++ Class29-Materials-2022/thunder-badge/js/main.js
@@ -1,2 +1,22 @@
 //Objects
 //Please create a pokemon class with as much detail as possible in 10 minutes
+
+class Pokemon {
+    constructor(age, power, colour, speed) {
+        this.age = age
+        this.power = power
+        this.colour = colour
+        this.speed = speed
+    }
+    shout() {
+        console.log(this.power)
+    }
+
+    run() {
+        console.log("I like running")
+    }
+}
+
+
+
+const pikachu = new Pokemon(7, 200, "brown", 30)
