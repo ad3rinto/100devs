765bc3f89e0c62960f7d8ad69c23d0c9abcb2022
ad3rinto <aad3rinto@gmail.com>
2023-07-11 09:00:21 +0100

    converting constructor to use class

--- Class30-Materials-2022/objects-espresso-machine/js/main.js
+++ Class30-Materials-2022/objects-espresso-machine/js/main.js
@@ -1,23 +1,42 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
 
-function EspressoMachine(colour, brand, capacity, powerRating) {
-    this.colour = colour
-    this.brand = brand
-    this.capacity = capacity
-    this.powerRating = powerRating
+// function EspressoMachine(colour, brand, capacity, powerRating) {
+//     this.colour = colour
+//     this.brand = brand
+//     this.capacity = capacity
+//     this.powerRating = powerRating
 
-    this.makeTea = function () {
+//     this.makeTea = function () {
+//         console.log("Making tea.....")
+//     }
+
+//     this.makeCoffee = function () {
+//         console.log("Making coffee.....")
+//     }
+//     this.makeHotChocolate = function () {
+//         console.log("Making hot chocolate.....")
+//     }
+// }
+
+class EspressoMachine {
+    constructor(colour, brand, capacity, powerRating) {
+        this.colour = colour
+        this.brand = brand
+        this.capacity = capacity
+        this.powerRating = powerRating
+    }
+    makeTea() {
         console.log("Making tea.....")
     }
 
-    this.makeCoffee = function () {
+    makeCoffee() {
         console.log("Making coffee.....")
     }
-    this.makeHotChocolate = function () {
+
+    makeHotChocolate() {
         console.log("Making hot chocolate.....")
     }
 }
-
 
 
 
