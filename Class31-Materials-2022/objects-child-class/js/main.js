59d1513f49d66076a82923e011d3167f2fd36da5
ad3rinto <aad3rinto@gmail.com>
2023-07-12 16:28:28 +0100

    looking at class and extention

--- Class31-Materials-2022/objects-child-class/js/main.js
+++ Class31-Materials-2022/objects-child-class/js/main.js
@@ -1,2 +1,25 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Animal {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+    makeSound() {
+        console.log(`I am a ${this.name}`)
+    }
+}
+
+
+class Dog extends Animal {
+    constructor(name, age, breed, colour) {
+        super(name, age)
+        this.breed = breed
+        this.colour = colour
+    }
+    makeSound() {
+        // super.makeSound()
+        console.log('na me oo')
+    }
+}
+
