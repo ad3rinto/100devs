af84db186281a26471e96ea5f692a65f2ef51321
ad3rinto <aad3rinto@gmail.com>
2023-07-31 15:30:13 +0100

    testing

--- Class35-Materials-2022/api-template/js/main.js
+++ Class35-Materials-2022/api-template/js/main.js
@@ -1,1 +1,12 @@
 //Make an api request using async await
+const houseOne = () => {
+    console.log("Paper delivered to house one ")
+}
+
+const houseTwo = () => {
+    console.log("Paper delivered to house Two ")
+}
+
+const houseThree = () => {
+    console.log("Paper delivered to house Three ")
+}
