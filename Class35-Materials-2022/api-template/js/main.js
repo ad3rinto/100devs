6f12c9623c460818cefd9e0443bbc7d360a91386
ad3rinto <aad3rinto@gmail.com>
2023-08-04 17:13:32 +0100

    intorduction to promises

--- Class35-Materials-2022/api-template/js/main.js
+++ Class35-Materials-2022/api-template/js/main.js
@@ -1,19 +1,33 @@
 //Make an api request using async await
-const houseOne = () => {
-    console.log("Paper delivered to house one ")
+
+function houseOne() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Paper delivered to house one")
+        }, 1000)
+    })
 }
 
-const houseTwo = () => {
-    setTimeout(() => console.log("Paper delivered to house Two "), 3000)
+
+function houseTwo() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Paper delivered to house two")
+        }, 5000)
+    })
 }
 
-const houseThree = () => {
-    console.log("Paper delivered to house Three ")
+function houseThree() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Paper delivered to house three")
+        }, 2000)
+    })
 }
 
 
 
 
 houseOne()
-houseTwo()
-houseThree()
+    .then(data => console.log(data))
+    .then(houseTwo).then(data => console.log(data)).then(houseThree).then(data => console.log(data)).catch(err => console.log(err))
